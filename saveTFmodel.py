2481eb140eb3dd83e5e292995c29b6b6cc69b09b
Gaëtan <gaetan.serre@universite-paris-saclay.fr>
2021-05-10 19:58:32 +0200

    Initial commit

--- saveTFmodel.py
+++ saveTFmodel.py
@@ -1,28 +1,3 @@
-# Neural Net
-
-Neural Net is a C++ library able to create a neural network from a file containing the weights.
-
-Currently, only the *Dense* layers and the *Relu* activation function are available but it's quite easy to add more.
-
-## Structure of the weight file
-
-```
-layer
-w1,1 w1,2 w1,3 b1
-w2,1 w2,2 w2,3 b2
-...
-layer
-w1,1 w1,2 w1,3 b1
-w2,1 w2,2 w2,3 b2
-...
-```
-With 
-+ *wi,d* the weight at dimension *d* for the neuron number *i*.
-+ *bi* the bias of the neuron number *i*.
-
-## Save your model
-If you are using Keras and Tensorflow, you can use this function to save your model into a file recognized by Neural Net:
-```python
 def saveTFModel(model, output_path):
     s = ""
     for layer in model.layers:
@@ -49,7 +24,3 @@ def saveTFModel(model, output_path):
     f = open(output_path, "w")
     f.write(s)
     f.close()
-```
-
-## License
-[GPL v3](https://choosealicense.com/licenses/gpl-3.0/)
